1c4148a3b0d1fbc9185466a15293c66895716fdb
Kobe Vo <kobevo05@gmail.com>
2025-10-29 15:52:48 -0700

    finished description js, need to finish dependant Tweet.getHTMLTableRow

--- js/descriptions.js
+++ js/descriptions.js
@@ -6,10 +6,31 @@ function parseTweets(runkeeper_tweets) {
 	}
 
 	//TODO: Filter to just the written tweets
+	tweet_array = runkeeper_tweets
+		.map(twt => new Tweet(twt.text, twt.created_at))
+		.filter(twt => twt.written);
 }
 
 function addEventHandlerForSearch() {
 	//TODO: Search the written tweets as text is entered into the search box, and add them to the table
+
+	document.getElementById("textFilter").addEventListener("input", (e) => {
+		const search = e.target.value.toLowerCase();
+		document.getElementById("searchText").innerText = e.target.value;
+
+		const filter = tweet_array.filter(twt => 
+			twt.text.toLowerCase().includes(search)
+		);
+
+		document.getElementById("searchCount").innerText = (search == "") ? 0: filter.length;
+
+		const table = document.getElementById("tweetTable");
+		table.innerHTML = "";
+		filter.forEach((index, twt) => {
+			table.innerHTML += twt.getHTMLTableRow(index + 1);
+		})
+
+	})
 }
 
 //Wait for the DOM to load
